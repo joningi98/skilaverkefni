406517ca9ff9ebde855a00d267e22a32c2494132
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-18 14:53:04 +0000

    Update project4.py

--- project4.py
+++ project4.py
@@ -1,21 +1,24 @@
 while True:
     try:
         shares = float(input("Enter number of shares: "))
+        while True:
+            try:
+                price, numerator, denominator = input("Enter price (dollars, numerator, denominator): ").split(' ')
+                price_int = float(price)
+                numerator_int = float(numerator)
+                denominator_int = float(denominator)
+                total_value = shares * (price_int + (numerator_int / denominator_int))
+                total_value = float(total_value)
+                print("%.2f" % total_value)
+                break
+            except ValueError:
+                print("Invalid price!")
+                continue
     except ValueError:
         print("Invalid number!")
-        break
-    try:
-        price, numerator, denominator = input("Enter price (dollars, numerator, denominator): ").split(' ')
-    except ValueError:
-        print("Invalid price!")
-        break
-    price_int = float(price)
-    numerator_int = float(numerator)
-    denominator_int = float(denominator)
-    total_value = shares * (price_int + (numerator_int / denominator_int))
-    total_value = float(total_value)
-    print("%.2f" % total_value)
+        continue
+
     resume = input("Continue: ")
-  
+
     if resume != 'y':
         break
