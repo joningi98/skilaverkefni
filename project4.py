1ef51168187f0698e73c04a44dff106362e14d80
Aron Hrafnsson <aronhr@users.noreply.github.com>
2018-09-19 23:06:41 +0000

    Update project4.py

--- project4.py
+++ project4.py
@@ -1,24 +1,34 @@
+def resume():
+    x = input("Continue: ")
+    if x != 'y':
+        quit()
+
+
+def calculate_total_value(shares):
+    while True:
+        try:
+            p, n, d = input("Enter price (dollars, numerator, denominator): ").split(' ')
+            price, numerator, denominator = float(p), float(n), float(d)
+            total_value = float(shares * (price + (numerator / denominator)))
+            total_value = ("%.2f" % total_value)
+            total_value = (str(round(shares)) + " shares with market price " + str(round(price)) + " " + str(round(numerator)) + "/" +
+                           str(round(denominator)) + " have value $" + str(total_value))
+            print(total_value)
+            break
+        except ValueError:
+            print("Invalid price!")
+
+
+def enter_shares():
+    while True:
+        try:
+            x = float(input("Enter number of shares: "))
+            calculate_total_value(x)
+            break
+        except ValueError:
+            print("Invalid number!")
+
+
 while True:
-    try:
-        shares = float(input("Enter number of shares: "))
-        while True:
-            try:
-                price, numerator, denominator = input("Enter price (dollars, numerator, denominator): ").split(' ')
-                price_int = float(price)
-                numerator_int = float(numerator)
-                denominator_int = float(denominator)
-                total_value = float(shares * (price_int + (numerator_int / denominator_int)))
-                total_value = ("%.2f" % total_value)
-                print(str(round(shares)) + " shares with market price " + str(price) + " " + str(numerator) + "/" + str(denominator) + " have value $" + str(total_value))
-                break
-            except ValueError:
-                print("Invalid price!")
-                continue
-    except ValueError:
-        print("Invalid number!")
-        continue
-
-    resume = input("Continue: ")
-
-    if resume != 'y':
-        break
+    enter_shares()
+    resume()
